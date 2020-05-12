46ef8a88b8d8eee780cf93855973685438336322
sureshkumarmylam <sureshkumarmylam@192.168.0.20>
2020-05-12 15:33:59 -0400

    updated

--- src/test/java/com/docker/test/DemoTest.java
+++ src/test/java/com/docker/test/DemoTest.java
@@ -1,30 +1,24 @@
 package com.docker.test;
 
 import org.testng.annotations.Test;
-import static org.testng.Assert.assertEquals;
-
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
 import org.testng.Assert;
-import org.testng.annotations.Test;
-
-import okhttp3.Challenge;
 
 public class DemoTest {
 
 	@Test
 	public void Test1() {
-//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/src/main/resources/drivers/chromedriver2");
+//		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/src/main/resources/drivers/chromedriver3");
 		System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir")+ "/chromedriver4");
 		ChromeOptions options = new ChromeOptions();
 		options.addArguments("--headless");
-		options.addArguments("start-maximized"); // open Browser in maximized mode
-		options.addArguments("disable-infobars"); // disabling infobars
-		options.addArguments("--disable-extensions"); // disabling extensions
-//		options.addArguments("--disable-gpu"); // applicable to windows os only
-		options.addArguments("--disable-dev-shm-usage"); // overcome limited resource problems
-		options.addArguments("--no-sandbox"); // Bypass OS security model
+		options.addArguments("start-maximized"); 
+		options.addArguments("disable-infobars"); 
+		options.addArguments("--disable-extensions");
+		options.addArguments("--disable-dev-shm-usage");
+		options.addArguments("--no-sandbox"); 
 		
 		WebDriver driver = new ChromeDriver(options);
 		driver.get("https://testdwh2.agmednet.net/");
